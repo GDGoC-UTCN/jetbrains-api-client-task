d7f9a4a3ad9f6a6099f629a3f9c5df28d0ecd723
Alex <alexmihoc@yahoo.com>
2026-02-23 20:16:37 +0200

    fix: add auto download

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,10 @@
 rootProject.name = "api-client"
 include("verification-framework")
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 pluginManagement {
     repositories {
         mavenCentral()
